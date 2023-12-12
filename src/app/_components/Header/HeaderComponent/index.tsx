54ae4c077a2511111e16f66931ca49e3b875aa05
Kenneth Aduan <kenneth.aduan10@gmail.com>
2023-12-12 11:49:59 +0800

    hambuger fix

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -8,6 +8,7 @@ import { usePathname } from 'next/navigation'
 import { Header } from '../../../../payload/payload-types'
 import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
+import { MobileNav } from '../MobileNav'
 import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
@@ -23,10 +24,16 @@ const HeaderComponent = ({ header }: { header: Header }) => {
     >
       <Gutter className={classes.wrap}>
         <Link href="/">
-          <Image src="/ATR_LOGO.png" alt="logo" width={80} height={5} />
+          <div className="flex justify-between space-x-2">
+            <Image src="/ATR_LOGO.png" alt="logo" width={80} height={5} />
+            <p className="hidden lg:block text-[#F2B10D] text-2xl lg:text-2xl sm:text-lg mt-4">
+              ATR Skin Care
+            </p>
+          </div>
         </Link>
 
         <HeaderNav header={header} />
+        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
--- src/app/_components/Header/MobileNav/index.tsx
+++ src/app/_components/Header/MobileNav/index.tsx
@@ -14,61 +14,111 @@ import classes from './index.module.scss'
 export const MobileNav: React.FC<{ header: HeaderType }> = ({ header }) => {
   const navItems = header?.navItems || []
   const { user } = useAuth()
-
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
   return (
-    <nav className={[classes.nav, user === undefined && classes.hide].filter(Boolean).join(' ')}>
-      {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="primary" invert={true} />
-      })}
-      {/* <CartLink /> */}
-      {user && (
-        <Link href="/account">
-          <div>
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              style={{ height: 30, width: 50 }}
+    <div className="lg:hidden">
+      <button
+        aria-label="Open Menu"
+        title="Open Menu"
+        className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+        onClick={() => setIsMenuOpen(true)}
+      >
+        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+          />
+          <path
+            fill="currentColor"
+            d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+          />
+          <path
+            fill="currentColor"
+            d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+          />
+        </svg>
+      </button>
+      {isMenuOpen && (
+        <div className="absolute top-0 left-0 w-full">
+          <div className="p-5 bg-white border rounded shadow-sm">
+            <div className="flex items-center justify-between mb-4">
+              <div>
+                <button
+                  aria-label="Close Menu"
+                  title="Close Menu"
+                  className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <nav
+              className={[classes.nav, user === undefined && classes.hide]
+                .filter(Boolean)
+                .join(' ')}
             >
-              <g id="SVGRepo_bgCarrier" strokeWidth={0} />
-              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
-              <g id="SVGRepo_iconCarrier">
-                <path
-                  d="M12.12 12.78C12.05 12.77 11.96 12.77 11.88 12.78C10.12 12.72 8.71997 11.28 8.71997 9.50998C8.71997 7.69998 10.18 6.22998 12 6.22998C13.81 6.22998 15.28 7.69998 15.28 9.50998C15.27 11.28 13.88 12.72 12.12 12.78Z"
-                  stroke="#188655"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+              {navItems.map(({ link }, i) => {
+                return <CMSLink key={i} {...link} appearance="primary" invert={true} />
+              })}
+              {/* <CartLink /> */}
+              {user && (
+                <Link href="/account">
+                  <div>
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ height: 30, width: 50 }}
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+                      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+                      <g id="SVGRepo_iconCarrier">
+                        <path
+                          d="M12.12 12.78C12.05 12.77 11.96 12.77 11.88 12.78C10.12 12.72 8.71997 11.28 8.71997 9.50998C8.71997 7.69998 10.18 6.22998 12 6.22998C13.81 6.22998 15.28 7.69998 15.28 9.50998C15.27 11.28 13.88 12.72 12.12 12.78Z"
+                          stroke="#188655"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M18.74 19.3801C16.96 21.0101 14.6 22.0001 12 22.0001C9.40001 22.0001 7.04001 21.0101 5.26001 19.3801C5.36001 18.4401 5.96001 17.5201 7.03001 16.8001C9.77001 14.9801 14.25 14.9801 16.97 16.8001C18.04 17.5201 18.64 18.4401 18.74 19.3801Z"
+                          stroke="#188655"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                          stroke="#188655"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </g>
+                    </svg>
+                  </div>
+                </Link>
+              )}
+              {!user && (
+                <Button
+                  el="link"
+                  href="/login"
+                  label="Login"
+                  appearance="primary"
+                  onClick={() => (window.location.href = '/login')}
                 />
-                <path
-                  d="M18.74 19.3801C16.96 21.0101 14.6 22.0001 12 22.0001C9.40001 22.0001 7.04001 21.0101 5.26001 19.3801C5.36001 18.4401 5.96001 17.5201 7.03001 16.8001C9.77001 14.9801 14.25 14.9801 16.97 16.8001C18.04 17.5201 18.64 18.4401 18.74 19.3801Z"
-                  stroke="#188655"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-                  stroke="#188655"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </g>
-            </svg>
+              )}
+              {user && <CartLink />}
+            </nav>
           </div>
-        </Link>
+        </div>
       )}
-      {!user && (
-        <Button
-          el="link"
-          href="/login"
-          label="Login"
-          appearance="primary"
-          onClick={() => (window.location.href = '/login')}
-        />
-      )}
-      {user && <CartLink />}
-    </nav>
+    </div>
   )
 }
